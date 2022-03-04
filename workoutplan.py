ac65cbb3f000976946993891b76804e87d1b855b
adkl1 <94471262+adkl1@users.noreply.github.com>
2022-03-04 16:39:30 +0200

    click prototype

--- workoutplan.py
+++ workoutplan.py
@@ -22,7 +22,8 @@ options_button = pygame.Rect(650,650,500,100)
 back_button = pygame.Rect(20,930,200,50)
 routin_button = pygame.Rect(1250,400,500,100)
 workout_sch_button = pygame.Rect(1250,250,500,100)
-
+workout_file_path_button = pygame.Rect(350,265,700,100)
+routin_file_path_button = pygame.Rect(350,450,700,100)
 #text
 font = pygame.font.Font(None, 50)
 def draw_text(text,font,color,surface,x,y):
@@ -34,7 +35,31 @@ def draw_text(text,font,color,surface,x,y):
     text_rect.topleft = (x,y)
     surface.blit(text_obj,text_rect)
 
+workout_file_path = "c:/python/workoutplan.txt"
+routin_file_path = "c:/python/workoutplan.txt"
+def change_text(text):
+    while True:
+        #exit on screen close
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
 
+        #mouse input
+        mx, my = pygame.mouse.get_pos()
+        if options_button.collidepoint((mx,my)):
+            if click:
+                options_menu()
+        if daily_plan_button.collidepoint((mx,my)):
+            if click:
+                daily_menu()
+                
+        click = False
+        pygame.display.update()
+        clock.tick(60)
 
 #main game loop
 def main_loop():
@@ -83,6 +108,8 @@ def options_menu():
     output: none
     '''
     running = True
+    global workout_file_path
+    click_on_text = False
     while running:
         click = False
         #exit on screen close
@@ -92,23 +119,44 @@ def options_menu():
                 pygame.quit()
                 exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+                if event.key == pygame.K_ESCAPE and not click_on_text:
                     running = False
+                elif event.key == pygame.K_KP_ENTER or event.key == pygame.K_ESCAPE:
+                    click_on_text = False
+                elif click_on_text and len(workout_file_path) < 30:
+                    if event.unicode.isalpha() or event.unicode == "/" or event.unicode == ".":
+                        workout_file_path += event.unicode
+                    elif event.key == pygame.K_BACKSPACE:
+                        workout_file_path = workout_file_path[:-1]
+                elif event.key == pygame.K_BACKSPACE:
+                        workout_file_path = workout_file_path[:-1]
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
+                    click_on_text = False
                     
         #mouse input
         mx, my = pygame.mouse.get_pos()
         if back_button.collidepoint((mx,my)):
             if click:
                 running = False #leave menu
-
+        if workout_file_path_button.collidepoint((mx,my)):
+            if click:
+                click_on_text = not click_on_text
+        
         #add images
         screen.blit(background,(0,0))
         draw_text("Options Menu", font,(0,0,0),screen,800,40)
         pygame.draw.rect(screen,(0,0,0),back_button)
         draw_text("Back",font,(255,255,255),screen,77,937)
+        #workout file field
+        pygame.draw.rect(screen,(0,0,0),workout_file_path_button)
+        draw_text("Workout File Path: ",font,(0,0,0),screen,20,300)
+        draw_text(workout_file_path,font,(255,255,255),screen,370,300)
+        #routine file field
+        pygame.draw.rect(screen,(0,0,0),routin_file_path_button)
+        draw_text("Routin File Path: ",font,(0,0,0),screen,20,475)
+        draw_text(routin_file_path,font,(255,255,255),screen,370,475)
 
         #loop necesities
         pygame.display.update()
